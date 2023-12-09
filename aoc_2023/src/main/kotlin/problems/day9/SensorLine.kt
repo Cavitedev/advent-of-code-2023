a1de1fc2816961de1e644b956d743f2523414446
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-12-09 09:06:31 +0100

    Refactor day 9

--- aoc_2023/src/main/kotlin/problems/day9/SensorLine.kt
+++ aoc_2023/src/main/kotlin/problems/day9/SensorLine.kt
@@ -3,24 +3,12 @@ package problems.day9
 
 data class SensorLine(val numbers: List<Long>) {
 
-    val extrapolations: MutableList<List<Long>> = mutableListOf()
     fun extrapolateForward(): Long {
-        return try {
-            extrapolations.add(numbers)
-            inlineExtrapolateForward(numbers)
-        } catch (e: Exception) {
-            numbers.last()
-        }
-
+        return inlineExtrapolateForward(numbers)
     }
 
     private fun inlineExtrapolateForward(numbersList: List<Long>): Long {
         val extrapolation = extrapolate(numbersList)
-        extrapolations.add(extrapolation)
-        if (extrapolation.isEmpty()) {
-            displayExtrapolations()
-            throw Exception("no prediction")
-        }
 
         if (extrapolation.all { it == 0L }) {
             return numbersList.last()
@@ -40,17 +28,6 @@ data class SensorLine(val numbers: List<Long>) {
         return returnList
     }
 
-    private fun displayExtrapolations(): String {
-        val initLength = extrapolations.first().size
-
-        val output = extrapolations.map { extrapolation ->
-            (" ".repeat(initLength - extrapolation.size)) + extrapolation.joinToString(" ")
-        }.joinToString("\n")
-        print(output)
-        return output
-    }
-
-
     fun extrapolateBackward(): Long {
         return inlineExtrapolateBackward(numbers)
     }
@@ -62,17 +39,6 @@ data class SensorLine(val numbers: List<Long>) {
         }
         return numbersList.first() - inlineExtrapolateBackward(extrapolation)
     }
-
-//    private fun extrapolateBackwards(numbersList: List<Long>): List<Long> {
-//
-//        val returnList: MutableList<Long> = mutableListOf()
-//
-//        for (i in numbersList.size - 1 downTo 1) {
-//            returnList.add(numbersList[i] - numbersList[i - 1])
-//        }
-//
-//        return returnList
-//    }
 
 
 }
